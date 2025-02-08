a18297218d712aa7f9380f2400286560e9ee60e3
Ryan Gosiaco <43663778+origamitoaster@users.noreply.github.com>
2025-02-08 01:58:45 -0800

    Update Identification.cs
    add support for TUF_GAMING_B850M_PLUS_WIFI

--- LibreHardwareMonitorLib/Hardware/Motherboard/Identification.cs
+++ LibreHardwareMonitorLib/Hardware/Motherboard/Identification.cs
@@ -617,6 +617,8 @@ internal class Identification
                 return Model.ROG_CROSSHAIR_X870E_HERO;
             case var _ when name.Equals("X11SWN-E", StringComparison.OrdinalIgnoreCase):
                 return Model.X11SWN_E;
+            case var _ when name.Equals("TUF GAMING B850M-PLUS WIFI", StringComparison.OrdinalIgnoreCase):
+                return Model.TUF_GAMING_B850M_PLUS_WIFI;
             case var _ when name.Equals("Base Board Product Name", StringComparison.OrdinalIgnoreCase):
             case var _ when name.Equals("To be filled by O.E.M.", StringComparison.OrdinalIgnoreCase):
                 return Model.Unknown;
